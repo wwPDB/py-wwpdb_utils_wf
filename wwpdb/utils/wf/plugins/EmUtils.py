9c6cd0c53be11c0a57c00b2c999d82156503c0c6
lucasemdb <lucas@ebi.ac.uk>
2024-05-24 12:19:48 +0100

    Remove commented blocks from `emmapcheck`

--- wwpdb/utils/wf/plugins/EmUtils.py
+++ wwpdb/utils/wf/plugins/EmUtils.py
@@ -174,37 +174,27 @@ class EmUtils(UtilsBase):
         """Run EM Map/Model validation on file upload"""
         try:
             (inpObjD, outObjD, _uD, _pD) = self._getArgs(kwArgs)
-            # in_map_file = inpObjD["src1"].getFilePathReference()
-            # in_half_map_file_1 = inpObjD["src2"].getFilePathReference()
-            # in_half_map_file_2 = inpObjD["src3"].getFilePathReference()
-            # in_model_file = inpObjD["src4"].getFilePathReference()
             input_file = inpObjD["src"].getFilePathReference()
             output_file = outObjD["dst"].getFilePathReference()
-            #
+
             cI = ConfigInfo()
             siteId = cI.get("SITE_PREFIX")
             dp = RcsbDpUtility(tmpPath=output_file, siteId=siteId, verbose=self._verbose, log=self._lfh)
             dp.setDebugMode(flag=True)
 
-            # dp.addInput(name="input_map_file_path", value=in_map_file)
-            # dp.addInput(name="input_half_map_file_path_1", value=in_half_map_file_1)
-            # dp.addInput(name="input_half_map_file_path_2", value=in_half_map_file_2)
-            # dp.addInput(name="input_model_file_path", value=in_model_file)
             dp.addInput(name="input_file_path", value=input_file)
             dp.addInput(name="output_file_path", value=output_file)
             dp.op("em-map-model-upload-check")
 
             if self._verbose:
-                # self._lfh.write("+EmUtils.emmapcheck() - input map  file path:  %s\n" % in_map_file)
-                # self._lfh.write("+EmUtils.emmapcheck() - input half map 1 file path:  %s\n" % in_half_map_file_1)
-                # self._lfh.write("+EmUtils.emmapcheck() - input half map 2 file path:  %s\n" % in_half_map_file_2)
-                # self._lfh.write("+EmUtils.emmapcheck() - input model file path:  %s\n" % in_model_file)
                 self._lfh.write("+EmUtils.emmapcheck() - input json file path:  %s\n" % input_file)
                 self._lfh.write("+EmUtils.emmapcheck() - output json file path:  %s\n" % output_file)
 
             if self.__cleanUp:
                 dp.cleanup()
+
             return True
+
         except Exception as _e:  # noqa: F841
             traceback.print_exc(file=self._lfh)
             return False
