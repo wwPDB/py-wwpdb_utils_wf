65abd8cf1c236b504a470b2601e7343250310246
Weslley Morellato Bueno <wbueno@ebi.ac.uk>
2025-08-18 09:16:38 -0400

    Moved data sync plugin to different module

---
+++ wwpdb/utils/wf/plugins/DepositUtils.py
@@ -0,0 +1,37 @@
+import sys
+import traceback
+
+from wwpdb.utils.wf.plugins.UtilsBase import UtilsBase
+from wwpdb.utils.config.ConfigInfo import ConfigInfo
+from wwpdb.io.misc.DataSync import DepositDataSync, SyncDirection, print_sync_result
+
+
+class DepositUtils(UtilsBase):
+    def __init__(self, verbose=False, log=sys.stderr):
+        super(DepositUtils, self).__init__(verbose, log)
+
+    def syncToDepositOp(self, **kwargs):
+        try:
+            (inpObjD, _outObjD, uD, _pD) = self._getArgs(kwargs)
+
+            dep_id = inpObjD["src"].getDepositionDataSetId()
+            config = ConfigInfo()
+
+            if not config.get("SITE_ARCHIVE_UI_STORAGE_PATH"):
+                self._lfh.write("+DepositUtils.syncToDepositOp No archive UI storage path configured. Skipping!\n")
+                return True
+
+            self._lfh.write("+DepositUtils.syncToDepositOp starting sync to deposit\n")
+
+            syncer = DepositDataSync()
+            result = syncer.sync_single(dep_id, SyncDirection.TO_DEPOSIT)
+            print_sync_result(result, self._lfh)
+
+            if not result['success']:
+                sys.exit(1)
+
+            return True
+        except Exception as _e:
+            if self._verbose:
+                traceback.print_exc(file=self._lfh)
+            return False
