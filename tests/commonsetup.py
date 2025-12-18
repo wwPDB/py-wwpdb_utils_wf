78a873cf26020bd6c2e335e74137a57296af3123
Ezra Peisach <ezra.peisach@rcsb.org>
2025-12-18 12:38:55 -0500

    Move tests to top level

--- tests/commonsetup.py
+++ tests/commonsetup.py
@@ -4,7 +4,7 @@ import os
 import platform
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(HERE)))
+TOPDIR = os.path.dirname(HERE)
 TESTOUTPUT = os.path.join(HERE, "test-output", platform.python_version())
 if not os.path.exists(TESTOUTPUT):
     os.makedirs(TESTOUTPUT)
